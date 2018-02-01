0b0c8d77dd46554b28de540619d12b8f779805f6
Mike Robinson <Mikerobinson1@gmail.com>
2018-01-31 19:42:15 -0500

    get rid of recompose (#11)

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {compose, setDisplayName} from 'recompose';
 import configureStore from './store/store';
 import {MemoryRouter} from 'react-router';
 import {AppShell} from './views/app-shell/app-shell';
 
-const enhance = compose(
-    setDisplayName('App'),
-);
-
 const store = configureStore();
-const component: React.SFC = () =>
+const App: React.SFC = () =>
     <Provider store={store}>
         <MemoryRouter>
                 <AppShell/>
         </MemoryRouter>
     </Provider>;
-
-const App = enhance(component);
 
 (window as any).getState = store.getState;
 
--- src/views/app-shell/app-shell.tsx
+++ src/views/app-shell/app-shell.tsx
@@ -1,19 +1,9 @@
 import * as React from 'react';
-import {ComponentEnhancer, compose, setDisplayName} from 'recompose';
 import {Route} from 'react-router';
 import './app-shell.scss';
-import {AppNav} from '../app-nav/app-nav';
-import {Hubs} from '../hubs/hubs';
 
-const enhance: ComponentEnhancer<{}, {}> = compose(
-    setDisplayName('App-Shell'),
-);
-
-const component: React.SFC = () =>
+export const AppShell: React.SFC = () =>
     <div className="app-shell">
-        <AppNav/>
-        <Route exact path="/" component={Hubs}/>
+        <Route exact path="/" render={() => <div>Home for now</div>}/>
         <Route path={'/hubs/:id'} render={({match: {params: {id}}}) => <p>{id}</p>}/>
     </div>;
-
-export const AppShell = enhance(component);
